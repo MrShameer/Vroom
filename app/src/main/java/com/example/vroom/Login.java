3ae810353290da07098737edfb596ef767c3b682
AnwarChong <AnwarBinBujang@users.noreply.github.com>
2021-11-09 01:10:19 +0800

    Login

--- app/src/main/java/com/example/vroom/Login.java
+++ app/src/main/java/com/example/vroom/Login.java
@@ -2,13 +2,25 @@ package com.example.vroom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
-
+    Button btn_signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn_signup = findViewById(R.id.btn_signup);
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
