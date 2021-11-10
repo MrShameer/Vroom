e9fcc531cf43b0e49d8fb94200d06ec0e9f5f7cb
AnwarChong <AnwarBinBujang@users.noreply.github.com>
2021-11-11 01:29:34 +0800

    Map Done

--- app/src/main/java/com/example/vroom/SignUp.java
+++ app/src/main/java/com/example/vroom/SignUp.java
@@ -1,12 +1,12 @@
 package com.example.vroom;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.vroom.api.Request;
 
