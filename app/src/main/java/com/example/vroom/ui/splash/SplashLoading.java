c8f8c8793cd45384fb0a9b8b2636c1ef0489b480
MrShameer <67554400+MrShameer@users.noreply.github.com>
2021-12-17 02:42:05 +0800

    mantap

---
+++ app/src/main/java/com/example/vroom/ui/splash/SplashLoading.java
@@ -0,0 +1,68 @@
+package com.example.vroom.ui.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+import com.example.vroom.Login;
+import com.example.vroom.MainActivity;
+import com.example.vroom.R;
+import com.example.vroom.api.Request;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.RequestBody;
+
+public class SplashLoading extends AppCompatActivity {
+    Request request = new Request();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_loading);
+        new mytask().execute();
+    }
+
+    private class mytask extends AsyncTask<Void,Void,Void> {
+        String respond;
+        JSONObject jsonObject = null;
+        Intent intent;
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            String token = "marhCW1SodbIZ5nAvigQo2BKM1Wymvpa5np2R0LH"; //BAGI TOKEN KT SINI. TOKEN NI TUK TEST JE EHH
+            RequestBody requestBody = RequestBody.create(null, new byte[0]);
+
+            respond = request.PostHeader(requestBody,getString(R.string.validate),token);
+            try {
+                jsonObject = new JSONObject(respond);
+                if (jsonObject.has("id")){
+                    System.out.println(jsonObject.getString("id"));//NI ID EHH SO STORE MANE2
+                    jsonObject.getString("name");
+                    jsonObject.getString("email");
+                    jsonObject.getString("role");
+                    jsonObject.getString("phone");
+                    //SEMUA NI STORE SEKALI EH
+
+                    Thread.sleep(2000); //saja nk bgi org tgok dulu
+                    intent = new Intent(SplashLoading.this, MainActivity.class);
+                }
+                else {
+                    Thread.sleep(2000);
+                    intent = new Intent(SplashLoading.this, Login.class);
+                }
+                startActivity(intent);
+                finish();
+            } catch (JSONException | InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+
+    }
+}
