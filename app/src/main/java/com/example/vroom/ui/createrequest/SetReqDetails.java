05eee390fb979195d2c00e6d319fc0ee0cce17fe
AnwarChong <AnwarBinBujang@users.noreply.github.com>
2021-12-23 11:41:04 +0800

    InsertReqDetails-new activity_req_details.xml and SetReqDetails Class

--- app/src/main/java/com/example/vroom/ui/createrequest/SetReqDetails.java
+++ app/src/main/java/com/example/vroom/ui/createrequest/SetReqDetails.java
@@ -1,16 +1,27 @@
 package com.example.vroom.ui.createrequest;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.vroom.R;
 
 public class SetReqDetails extends AppCompatActivity {
-
+    ImageButton btn_back;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_req_details);
+
+        //setup Back Button
+        btn_back=findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finishAndRemoveTask();
+            }
+        });
     }
 
 }
