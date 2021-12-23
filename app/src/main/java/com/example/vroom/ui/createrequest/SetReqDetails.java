e8980747a34a8d9e59a2a1f1ec335d1630fa67a0
AnwarChong <AnwarBinBujang@users.noreply.github.com>
2021-12-24 00:04:40 +0800

    ReqDone

--- app/src/main/java/com/example/vroom/ui/createrequest/SetReqDetails.java
+++ app/src/main/java/com/example/vroom/ui/createrequest/SetReqDetails.java
@@ -1,8 +1,10 @@
 package com.example.vroom.ui.createrequest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +12,7 @@ import com.example.vroom.R;
 
 public class SetReqDetails extends AppCompatActivity {
     ImageButton btn_back;
+    TextView btn_req;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_req_details);
@@ -20,6 +23,15 @@ public class SetReqDetails extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finishAndRemoveTask();
+            }
+        });
+
+        btn_req=findViewById(R.id.btn_req);
+        btn_req.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(SetReqDetails.this,ReqFinish.class);
+                startActivity(intent);
             }
         });
     }
