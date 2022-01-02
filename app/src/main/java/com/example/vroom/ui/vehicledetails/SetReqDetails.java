61cb86f2990f9aabdf113874bfd8ae407231bfb5
AnwarChong <AnwarBinBujang@users.noreply.github.com>
2022-01-02 23:34:03 +0800

    Date

--- app/src/main/java/com/example/vroom/ui/vehicledetails/SetReqDetails.java
+++ app/src/main/java/com/example/vroom/ui/vehicledetails/SetReqDetails.java
@@ -1,14 +1,18 @@
 package com.example.vroom.ui.vehicledetails;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import com.example.vroom.R;
 import com.example.vroom.api.Request;
@@ -18,17 +22,32 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 
+import java.text.DateFormat;
+import java.util.Calendar;
+
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 
 public class SetReqDetails extends AppCompatActivity {
     ImageButton btn_back;
-    TextView btn_req;
+    public TextView btn_req;
+    public static TextView tv_startdate,tv_enddate;
+    public TextView tv_pickuplocation;
+    public TextView tv_pickupdate;
+    public TextView tv_cost;
+    String selectedDate;
     Intent intent;
     Request request = new Request();
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_details);
+
+        tv_startdate=findViewById(R.id.tv_startdate);
+        tv_enddate=findViewById(R.id.tv_enddate);
+        tv_pickuplocation=findViewById(R.id.tv_pickuplocation);
+        tv_pickupdate=findViewById(R.id.tv_pickupdate);
+        tv_cost=findViewById(R.id.tv_cost);
+
 
         intent=getIntent();
         //setup Back Button
@@ -45,6 +64,22 @@ public class SetReqDetails extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 new mytask().execute();
+            }
+        });
+
+        tv_startdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new DatePickerFragment("start");
+                newFragment.show(getSupportFragmentManager(), "datePicker");
+
+            }
+        });
+        tv_enddate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment = new DatePickerFragment("end");
+                newFragment.show(getSupportFragmentManager(), "datePicker");
             }
         });
     }
@@ -72,13 +107,50 @@ public class SetReqDetails extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-                Toast.makeText(getApplicationContext(),respond,Toast.LENGTH_SHORT).show();
-                if (respond.contains("Sucessfully")){
-                    Intent intent=new Intent(SetReqDetails.this, VehicleSucessfull.class);
-                    startActivity(intent);
-                }
-                finish();
+            Toast.makeText(getApplicationContext(),respond,Toast.LENGTH_SHORT).show();
+            if (respond.contains("Sucessfully")){
+                Intent intent=new Intent(SetReqDetails.this, VehicleSucessfull.class);
+                startActivity(intent);
+            }
+            finish();
         }
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+        TextView tv1;
+        public DatePickerFragment(String selectedDate) {
+            if(selectedDate.equals("start")){
+                tv1=SetReqDetails.tv_startdate;
+
+            }else
+                tv1=SetReqDetails.tv_enddate;
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar calendar = Calendar.getInstance();
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Create a Calendar instance
+            Calendar mCalendar = Calendar.getInstance();
+            // Set static variables of Calendar instance
+            mCalendar.set(Calendar.YEAR,year);
+            mCalendar.set(Calendar.MONTH,month);
+            mCalendar.set(Calendar.DAY_OF_MONTH,day);
+            // Get the date in form of string
+            String selectedDate = DateFormat.getDateInstance(DateFormat.FULL).format(mCalendar.getTime());
+            // Set the textview to the selectedDate String
+            tv1.setText(selectedDate);
+        }
+
     }
 
 }
